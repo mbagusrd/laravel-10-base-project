090f62dd874840c39d829afa356c172aceae6371
bagus <mbagusrd@gmail.com>
2023-11-24 06:53:36 +0700

    add User resource for filament

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+use function Laravel\Prompts\password;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required(),
+                TextInput::make('email')->required()->email(),
+                TextInput::make('password')->password()->disableAutocomplete()->confirmed()
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state)) /* hash password yang diinput */
+                    ->dehydrated(fn ($state) => filled($state)) /* hanya update jika ada value dalam field */
+                    ->required(fn (String $context): bool => ($context == "create")), /* required jika form dalam context create */
+                TextInput::make('password_confirmation')->password()->disableAutocomplete()
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn (String $context): bool => ($context == "create")),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make("name"),
+                TextColumn::make("email")
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
