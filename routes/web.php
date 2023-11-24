7b78e57b837135d88b91c32bc9cc5b594c366972
bagus <mbagusrd@gmail.com>
2023-11-24 15:56:47 +0700

    add login named route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Filament\Http\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin/login', Login::class)->name('login');
